2cc5099395974cf3aafa20c3470c8a8ed0dccd00
404RequestedUserNotFound <ahmedshafinshafi@gmail.com>
2023-07-11 18:49:14 +0600

    All blog and blog by id done and added

---
+++ my-blog/app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = BlogPost::all(); //fetch all blog posts from DB
+        return view('blog.index', [
+                'posts' => $posts,
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('blog.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BlogPost $blogPost)
+    {
+        return view('blog.show', [
+            'post' => $blogPost,
+        ]); //returns the view with the post
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BlogPost $blogPost)
+    {
+        //
+    }
+}
