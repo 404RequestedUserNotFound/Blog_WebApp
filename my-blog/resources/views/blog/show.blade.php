04c8d49b39e6893a119f2839a9d98543b12c930e
404RequestedUserNotFound <ahmedshafinshafi@gmail.com>
2023-07-13 21:25:23 +0600

    Fix in index of blog view.

--- my-blog/resources/views/blog/show.blade.php
+++ my-blog/resources/views/blog/show.blade.php
@@ -17,34 +17,4 @@
             </div>
         </div>
     </div>
-
-    <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card border border-dark" style="border-width: 3px;">
-                <div class="card-header">{{ Auth::user()->name }}, you are logged in! You can share your thoughts now.
-
-                <a href="/comment/create/post" class="btn btn-primary btn-sm">Post Blog</a>
-
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-8">
-                            <h1 class="display-one">Posted Blogs!</h1>
-                            <p>  </p>
-                        </div>
-
-                    </div>
-                    @forelse($posts as $post)
-                    <ul>
-                        <li><a href="./comment/{{ $post->id }}">{{ ucfirst($post->comment) }}</a></li>
-                    </ul>
-                    @empty
-                    <p class="text-warning">No blog Posts available</p>
-                    @endforelse
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
